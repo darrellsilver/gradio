0fb283ea1e7059ddc0f22ac0846b1a4f5e00f804
darrellsilver <24387+darrellsilver@users.noreply.github.com>
2023-05-06 18:49:43 -0400

    next attempt at replacing canvas on select

--- js/gallery/src/Gallery.svelte
+++ js/gallery/src/Gallery.svelte
@@ -97,7 +97,7 @@
 
 
 
-				const myContext = $("canvas[key='temp']").getContext("2d");
+				const myContext = document.querySelectorAll("canvas[key='temp']")[0].getContext("2d");
 				const img = new Image();
 				img.src = 'https://cdn.vox-cdn.com/thumbor/Si2spWe-6jYnWh8roDPVRV7izC4=/0x0:1192x795/1400x788/filters:focal(596x398:597x399)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg';
 				img.onload = () => {
